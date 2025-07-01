e63a81612f90a9d7a0ca7441fba9fac8c84db192
Diego Trejo <diego_trejo@hotmail.com>
2025-07-01 12:34:23 -0500

    Primeros ajustes de mi aplicacion.  Que nervios !!!

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // This is a simple console application that prints "Hello, World!" to the console.
+            // It is designed to run on .NET 8.0 with C# 12.0 language features.
+            // The project file specifies the target framework and language version.
+            var s = "Hello, World!";
+            Console.WriteLine(s);
         }
     }
 }
