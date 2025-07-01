ca276c9972d6cefae0d21f8f471972c9d91502aa
Diego Trejo <diego_trejo@hotmail.com>
2025-07-01 12:52:17 -0500

    cambios de ejemplo

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,12 @@
             // It is designed to run on .NET 8.0 with C# 12.0 language features.
             // The project file specifies the target framework and language version.
             var s = "Hello, World!";
+
+
+
+
+
+
             Console.WriteLine(s);
         }
     }
